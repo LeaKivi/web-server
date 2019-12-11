4de33c8091a813f8e00e1e663ffaf904c9f49da2
Katalina Kivinen <katalina@mios.fi>
2019-12-11 19:14:23 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, '../public/')))
+
+app.get('', (req, res)=>{
+    res.render('index', {
+        title: 'Weatherapp',
+        name: 'Katalina Kivinen'
+    })
+})
+
+app.get('', (req, res)=>{
+    res.render('about', {
+        title: 'About me',
+        name: 'Katalina Kivinen'
+    })
+})
+
+app.get('/weather', (req, res)=>{
+    res.send({
+        location: 'Espoo',
+        forecast: 'Not too nice wather. Wet and cold :('
+    })
+})
+
+// app.com => show homepage
+// app.com/help
+// app.com/about
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+})
